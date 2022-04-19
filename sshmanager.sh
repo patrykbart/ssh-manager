c4b6520cdca980ae8eed4ca521044b62125fd3f3
Patryk Bartkowiak <patryk@pieces.app>
2022-04-19 18:03:53 +0200

    added utility functions

--- sshmanager.sh
+++ sshmanager.sh
@@ -71,13 +71,143 @@ function run_menu()  # size, selected_item, ...name, ...address
 	done
 }
 
-selected_item=0
+function list_connections() # size, ...names, ...address
+{
+  local function_arguments=($@)
+
+  local size="$1"
+  local name=(${function_arguments[@]:1:$size})
+  local address=(${function_arguments[@]:1+$size})
+
+  if [ $size = 0 ] ; then
+    echo
+    echo "There are no servers!" >&2
+    exit 1
+  else
+    for (( i = 0; i < $size; ++i ))
+      do
+        printf "%s\t%s\n" "${name[i]}" "${address[i]}" | expand -t 30
+      done
+  fi
+}
+
+function add_connection() # size, ...names
+{
+  local function_arguments=($@)
+
+  local size="$1"
+  local names=(${function_arguments[@]:1})
+
+  printf "server name: "
+  read -r name
+  printf "user: "
+  read -r user
+  printf "IP address: "
+  read -r address
+
+  if [[ " ${names[*]} " =~ " ${name} " ]]; then
+      echo
+      echo "[ERROR] This name is already used!" >&2
+      exit 1
+  else
+    printf "\n$name $user $address" >> "$HOME/.sshrc"
+  fi
+}
+
+function remove_connection() # size, ...names, ...users, ...addresses
+{
+  local function_arguments=($@)
+
+  local size="$1"
+  local names=(${function_arguments[@]:1:$size})
+  local users=(${function_arguments[@]:1+$size:1+2*$size})
+  local addresses=(${function_arguments[@]:1+2*$size})
+
+  printf "server name: "
+  read -r name
+
+  if [[ " ${names[*]} " =~ " ${name} " ]]; then
+    rm "$HOME/.sshrc"
+    touch "$HOME/.sshrc"
+
+    for (( i = 0; i < $size; ++i ))
+    do
+      if [ "$name" != "${names[i]}" ] ; then
+        printf "\n${names[i]} ${users[i]} ${addresses[i]}" >> "$HOME/.sshrc"
+      fi
+    done
+
+    echo
+    echo "Removed ${name}!"
+  else
+    echo
+    echo "[ERROR] There is no server with name ${name}!" >&2
+    exit 1
+  fi
+}
+
+function update_connection() # size, ...names, ...users, ...addresses
+{
+  local function_arguments=($@)
+
+  local size="$1"
+  local names=(${function_arguments[@]:1:$size})
+  local users=(${function_arguments[@]:1+$size:1+2*$size})
+  local addresses=(${function_arguments[@]:1+2*$size})
+
+  printf "server name: "
+  read -r name
+  printf "new user: "
+  read -r user
+  printf "new IP address: "
+  read -r address
+
+  if [[ " ${names[*]} " =~ " ${name} " ]]; then
+    rm "$HOME/.sshrc"
+    touch "$HOME/.sshrc"
+
+    for (( i = 0; i < $size; ++i ))
+    do
+      if [ "$name" != "${names[i]}" ] ; then
+        printf "\n${names[i]} ${users[i]} ${addresses[i]}" >> "$HOME/.sshrc"
+      else
+        printf "\n$name $user $address" >> "$HOME/.sshrc"
+      fi
+    done
+
+    echo
+    echo "Updated ${name}!"
+  else
+    echo
+    echo "[ERROR] There is no server with name ${name}!" >&2
+    exit 1
+  fi
+}
+
+touch "$HOME/.sshrc"
 declare -a name=( $(cut -d " " -f1 $HOME/.sshrc) )
 declare -a user=( $(cut -d " " -f2 $HOME/.sshrc) )
 declare -a address=( $(cut -d " " -f3 $HOME/.sshrc) )
 
-run_menu "${#name[@]}" "$selected_item" "${name[@]}" "${address[@]}"
-menu_result="$?"
+if [ "$1" = "list" ] ; then
+  list_connections "${#name[@]}" "${name[@]}" "${address[@]}"
+elif [ "$1" = "add" ] ; then
+  add_connection "${#name[@]}" "${name[@]}"
+elif [ "$1" = "remove" ] ; then
+  remove_connection "${#name[@]}" "${name[@]}" "${user[@]}" "${address[@]}"
+elif [ "$1" = "update" ] ; then
+  update_connection "${#name[@]}" "${name[@]}" "${user[@]}" "${address[@]}"
+elif [ "$#" = 0 ] ; then
+  selected_item=0
 
-printf "Connecting to ${name[$menu_result]}...\n"
-ssh "${user[$menu_result]}"@"${address[$menu_result]}"
+  run_menu "${#name[@]}" "$selected_item" "${name[@]}" "${address[@]}"
+  menu_result="$?"
+
+  echo
+  printf "Connecting to ${name[$menu_result]}...\n"
+  ssh "${user[$menu_result]}"@"${address[$menu_result]}"
+else
+  echo
+  echo "[ERROR] Wrong arguments!" >&2
+  exit 1
+fi
